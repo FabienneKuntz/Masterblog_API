475a362767500e8291f8aa2d94952b221dcdfb42
Fabienne Kuntz <fabienne.kuntz04@gmail.com>
2026-01-28 17:49:02 +0100

    Add Swagger documentation for all API endpoints

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -2,6 +2,7 @@ from idlelib.macosx import hideTkConsole
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -149,6 +150,19 @@ def search_posts():
         ]
 
     return jsonify(results)
+
+
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Masterblog API' # (3) You can change this if you like
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 
 if __name__ == '__main__':
